418fa03bac82d41ece936c29e9439f7bf91e38bd
tau-tao <tau-tao@github.com>
2017-01-19 19:57:28 +1100

    Connects...

--- test/FtdiTest1.hs
+++ test/FtdiTest1.hs
@@ -1,12 +1,19 @@
-import Protolude
-import LibFtdi (ftdiInit, ftdiDeInit)
-import Data.Text as T
+import           Data.Text as T
+import           LibFtdi   (DeviceHandle, ftdiDeInit, ftdiInit, ftdiUSBClose,
+                            ftdiUSBOpen, ftdiUSBReset, withFtdi)
+import           Protolude
+
+doStuff :: DeviceHandle -> IO ()
+doStuff d = do
+  ftdiUSBOpen d (0x09fb, 0x6001)
+  ftdiUSBReset d
+  putStrLn ("Init OK" :: Text)
+  ftdiUSBClose d
+  ftdiDeInit d
 
 main :: IO ()
 main = do
-  dh <- ftdiInit
-  case dh of
-    Left err -> putStrLn $ T.pack $ "Error:" ++ show err
-    Right d -> do
-      putStrLn ("Init OK" :: Text)
-      ftdiDeInit d
+    dh <- ftdiInit
+    case dh of
+      Left err -> putStrLn $ T.pack $ "Error:" ++ show err
+      Right _ -> withFtdi doStuff
